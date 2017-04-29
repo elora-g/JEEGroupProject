a8574e44a4ef7afead2fe3267bf88304a34b4059
Elora <elora.guyader@gmail.com>
2017-04-29 23:42:53 +0200

    servlets definition and mapping in web.xml

--- src/com/jeegroupproject/servlets/advisor/ClientServlet.java
+++ src/com/jeegroupproject/servlets/advisor/ClientServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class clientServlet
  */
-@WebServlet("/clientServlet")
+@WebServlet("/ClientServlet")
 public class ClientServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
